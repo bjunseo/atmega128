ca810fd81e426c7ac1b1974efd35b169ede63916
leejunseo <junseo010234@gmail.com>
2020-11-11 14:12:59 +0900

    d0ㅅ0b

--- LaserModuale_1111/LaserModuale_1111/main.c
+++ LaserModuale_1111/LaserModuale_1111/main.c
@@ -17,52 +17,30 @@ void uart_write(unsigned char data)
 	UDR0 = data;
 }
 
-void adcInit(void)  //ADC 초기화 함수
-{
-	ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)); //16Mhz/128 = 125Khz
-	ADMUX |= (1<<REFS0);       //AVCC(5V)
-	ADCSRA |= (1<<ADEN);      //ADC 인에이블
-}
-
-uint16_t readAdc(uint8_t channel)  //값을 읽어오는 함수
-{
-	
-	ADMUX &= 0xF0;
-	ADMUX |= channel;
-	
-	ADCSRA |= (1<<ADSC);      //변환 시작
-	while(ADCSRA&(1<<ADSC));//변환 완료되기를 기다림.
-	
-	return ADCW;  //ADC값 반환
-}
-
 int main(void)
 {
-	int ch_ADC;
     /* Replace with your application code */
 	uart_init(BAUDRATE(9600));
-	
 	DDRA = 0xFF;
 	DDRC = 0x00;
 	DDRB = 0xFF;
 
-	adcInit();
-		
 	PORTB = 0x01;
 	_delay_ms(3000);
 	
     while (1) 
     {
-		PORTA = 0x01;
-		ch_ADC = readAdc(0);	
+		PORTA = 0x01;	
 		if((PINC & 0x01) == 0x00)
 		{
 			PORTB = 0x04;
+			uart_write(1);
 		}
 		else
 		{
 			PORTB = 0x02;
+			uart_write(2);
 		}
-		uart_write(ch_ADC);
+		_delay_ms(1000);
     }
 }
---
+++ LaserModuale_1111/gui/Obstacle detection/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Media;
+
+namespace Obstacle_detection
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort;
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] Portnom = SerialPort.GetPortNames();
+
+            foreach (string portnum in Portnom)
+            {
+                PortList.Items.Add(portnum);
+            }
+ 
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            _serialPort = new SerialPort();
+            if (_serialPort.IsOpen == false)
+            {
+                _serialPort.PortName = PortList.SelectedItem.ToString();
+                _serialPort.BaudRate = 9600;
+                _serialPort.DataBits = (int)8;
+                _serialPort.Parity = Parity.None;
+                _serialPort.StopBits = StopBits.One;
+                _serialPort.ReadTimeout = (int)500;
+                _serialPort.WriteTimeout = (int)500;
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+
+                _serialPort.Open();
+
+                status.Text = _serialPort.PortName + "연결되었습니다.";
+            }
+            else
+                status.Text = _serialPort.PortName + "연결되어 있습니다.";
+        }
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(MySerialReceived));
+            //invoke
+        }
+        private void MySerialReceived(object s, EventArgs e)
+        {
+            string ReceiveData = _serialPort.ReadChar().ToString();
+            Receive.Text = ReceiveData;
+            if (ReceiveData == "1")
+            {
+               Thread thread = new+
+                playWP();
+                this.BackColor = Color.Red;
+                ssss.Text = "침입감지";
+            }
+            if (ReceiveData == "2")
+            {
+                this.BackColor = Color.Blue;
+                ssss.Text = "침입감지중";
+            }
+        }
+        
+        private void playWP()
+        {
+            
+            SoundPlayer wp1 = new SoundPlayer("C:/Users/user/Documents/Atmel Studio/7.0/schoolbook/LaserModuale_1111/gui/녹음.wav");
+            wp1.PlaySync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
